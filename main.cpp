925d254cca4c58697188408c331b647e84a7815d
Dillon Huff <dillonhuff@gmail.com>
2017-08-25 11:04:48 -0700

    All connections are ordered

--- main.cpp
+++ main.cpp
@@ -9,6 +9,113 @@ bool isSelect(Wireable* fst) {
   return fst->getKind() == Wireable::WK_Select;
 }
 
+void print_wireable_selects(Wireable* fst_select) {
+  cout << "Wireable selects" << endl;
+  for (auto& s : fst_select->getSelects()) {
+    Wireable* w = s.second;
+    assert(isSelect(w));
+    Select* sel = static_cast<Select*>(w);
+    Type* tp = sel->getType();
+    
+    cout << s.first << " matches " << sel->wireableKind2Str(sel->getKind()) << " with type = " << tp->toString() << endl;
+  }
+  cout << "End selects" << endl;
+}
+
+bool connection_is_ordered(const Connection& connection) {
+  Wireable* fst = connection.first;
+  Wireable* snd = connection.second;
+
+  assert(isSelect(fst));
+  assert(isSelect(snd));
+
+  // Is this the same as fst->getParent()->getType() ?? No, I dont think so
+  Type* fst_tp = fst->getType();
+  Type* snd_tp = snd->getType();
+
+  if ((fst_tp->isInput() && snd_tp->isOutput()) ||
+      (fst_tp->isOutput() && snd_tp->isInput())) {
+    return true;
+  }
+
+  return false;
+}
+
+void print_ordered_connections(Module* add4_n) {
+  cout << "Ordered connections" << endl;
+  for (auto& connection : add4_n->getDef()->getConnections()) {
+    cout << "---- CONNECTION" << endl;
+    Wireable* fst = connection.first;
+    cout << "First wireable kind = " << fst->wireableKind2Str(fst->getKind()) << endl;
+    cout << "con first = " << connection.first->toString() << endl;
+
+    Wireable* snd = connection.second;
+    cout << "con second = " << connection.second->toString() << endl;
+
+    assert(isSelect(fst));
+    assert(isSelect(snd));
+
+    Select* fst_select = static_cast<Select*>(fst);
+    Select* snd_select = static_cast<Select*>(snd);
+
+    Type* fst_tp = fst_select->getType();
+    Type* snd_tp = snd_select->getType();
+
+    cout << "fst type = " << fst_tp->toString() << endl;
+    cout << "snd type = " << snd_tp->toString() << endl;
+
+    assert(connection_is_ordered(connection));
+
+    // cout << "fst parent ptr = " << fst_select->getParent() << endl;
+    // print_wireable_selects(fst_select->getParent());
+    // cout << "snd parent ptr = " << snd_select->getParent() << endl;
+    // print_wireable_selects(snd_select->getParent());
+  }
+  
+}
+
+void print_connections(Module* add4_n) {
+  cout << "Instance ptrs" << endl;
+  for (auto& inst : add4_n->getDef()->getInstances()) {
+    cout << inst.first << " = " << inst.second << endl;
+
+    Instance* ist = inst.second;
+    if (ist->hasConfigArgs()) {
+      cout << "--- Config args" << endl;
+      for (auto& arg : ist->getConfigArgs()) {
+      }
+    }
+
+    for (auto& arg : ist->getGenArgs()) {
+      cout << "Gen Arg" << endl;
+      Arg* ag = arg.second;
+      cout << ag->toString() << endl;
+    }
+  }
+  cout << "My connections printout" << endl;
+  for (auto& connection : add4_n->getDef()->getConnections()) {
+    cout << "---- CONNECTION" << endl;
+    Wireable* fst = connection.first;
+    cout << "First wireable kind = " << fst->wireableKind2Str(fst->getKind()) << endl;
+    cout << "con first = " << connection.first->toString() << endl;
+
+    Wireable* snd = connection.second;
+    cout << "con second = " << connection.second->toString() << endl;
+
+    assert(isSelect(fst));
+    assert(isSelect(snd));
+
+    Select* fst_select = static_cast<Select*>(fst);
+    Select* snd_select = static_cast<Select*>(snd);
+
+    cout << "fst parent ptr = " << fst_select->getParent() << endl;
+    print_wireable_selects(fst_select->getParent());
+    cout << "snd parent ptr = " << snd_select->getParent() << endl;
+    print_wireable_selects(snd_select->getParent());
+  }
+
+}
+
 int main() {
   uint n = 32;
   
@@ -50,42 +157,7 @@ int main() {
   cout << "After running generators" << endl;
   add4_n->print();
 
-  cout << "Instance ptrs" << endl;
-  for (auto& inst : add4_n->getDef()->getInstances()) {
-    cout << inst.first << " = " << inst.second << endl;
-
-    Instance* ist = inst.second;
-    if (ist->hasConfigArgs()) {
-      cout << "--- Config args" << endl;
-      for (auto& arg : ist->getConfigArgs()) {
-      }
-    }
-
-    for (auto& arg : ist->getGenArgs()) {
-      cout << "Gen Arg" << endl;
-      Arg* ag = arg.second;
-      cout << ag->toString() << endl;
-    }
-  }
-  cout << "My connections printout" << endl;
-  for (auto& connection : add4_n->getDef()->getConnections()) {
-    cout << "---- CONNECTION" << endl;
-    Wireable* fst = connection.first;
-    cout << "First wireable kind = " << fst->wireableKind2Str(fst->getKind()) << endl;
-    cout << "con first = " << connection.first->toString() << endl;
-
-    Wireable* snd = connection.second;
-    cout << "con second = " << connection.second->toString() << endl;
-
-    assert(isSelect(fst));
-    assert(isSelect(snd));
-
-
-    Select* fst_select = static_cast<Select*>(fst);
-    Select* snd_select = static_cast<Select*>(snd);
-    cout << "fst parent ptr = " << fst_select->getParent() << endl;
-    cout << "snd parent ptr = " << snd_select->getParent() << endl;
-  }
+  print_ordered_connections(add4_n);
 
   deleteContext(c);
   
