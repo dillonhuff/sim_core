5e14acc85e69fc103d1b0bf5328658a9b4e0f5f2
Dillon Huff <dillonhuff@gmail.com>
2017-08-30 16:01:06 -0700

    Creating a test of masking

--- gencode/test_and37.c
+++ gencode/test_and37.c
@@ -1,5 +1,25 @@
 #include "and37.h"
 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
 int main() {
-  return 0;
+  uint64_t one = 1;
+
+  uint64_t ins[2];
+  ins[0] = (one << 36) | (one << 37) | (one << 60);
+  ins[1] = (one << 36) | (one << 37) | (one << 60);
+
+  uint64_t out;
+
+  simulate(ins, &out);
+
+  // NOTE: This 37th and 60th bits should be masked away by the simulator
+  uint64_t expected = one << 36;
+  
+  printf("and out  = %llu\n", out);
+  printf("expected = %llu\n", expected);
+
+  return expected == out;
 }
