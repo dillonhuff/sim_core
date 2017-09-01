d10fe576ce2e5745445246085e0a93ad6dd5972a
Dillon Huff <dillonhuff@gmail.com>
2017-09-01 10:54:59 -0700

    Pulling apart graph code

--- src/sim.hpp
+++ src/sim.hpp
@@ -1,21 +1,9 @@
 #pragma once
 
 #include "wire_node.hpp"
-
-#include <boost/graph/directed_graph.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/topological_sort.hpp>
+#include "op_graph.hpp"
 
 namespace sim_core {
-
-  typedef WireNode WireableNode;
-
-  typedef std::pair<WireableNode, WireableNode> Conn;
-  typedef boost::property<boost::edge_name_t, Conn > EdgeProp;
-  typedef boost::directed_graph<boost::property<boost::vertex_name_t, WireableNode>, EdgeProp > NGraph;
-
-  typedef boost::graph_traits<NGraph>::vertex_descriptor vdisc;
-  typedef boost::graph_traits<NGraph>::edge_descriptor edisc;
 
   void buildOrderedGraph(CoreIR::Module* mod, NGraph& g);
 
